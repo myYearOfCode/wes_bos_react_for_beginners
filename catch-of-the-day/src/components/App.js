65de2f53f030c3f82f6b266993ee3caa726325e1
Ross <Ross@myyearofcode.com>
2018-11-29 21:25:25 -0500

    add inventory in state and enable addToOrder

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from "../sample-fishes";
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -15,14 +17,34 @@ class App extends React.Component {
     fishes[`${Date.now()}`] = fish;
     this.setState({fishes});
   }
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes})
+  }
+  addToOrder = key => {
+    //take a copy of state order
+    const order = {...this.state.order };
+    //either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // call setstate to update our state Object
+    this.setState({ order })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                addToOrder={this.addToOrder}
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+              />))}
+          </ul>
         </div>
         <Order />
-        <Inventory fishHandler={this.addFish}/>
+        <Inventory fishHandler={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
